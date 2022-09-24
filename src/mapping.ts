534d4e48b3f19a00014098c71b29c65f236727b9
halink0803 <halink0803@gmail.com>
2022-09-24 09:04:35 +0700

    update mapping logic to fix id conflict

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,7 +4,7 @@ import {
 import { EmergencyWithdrawInfo } from "../generated/schema"
 
 export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {
-  let info = new EmergencyWithdrawInfo(event.transaction.hash.toHex())
+  let info = new EmergencyWithdrawInfo(event.transaction.hash.toHex()+event.params.nftId.toString())
 
   info.number = event.block.number
   info.timestamp = event.block.timestamp
